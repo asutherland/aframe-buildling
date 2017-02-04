e8aa12dbe96e666fa1b5f50f82c0875fbf814897
Andrew Sutherland <asutherland@asutherland.org>
2017-02-03 22:02:29 -0500

    round window placer

--- src/obj_placers/window_placer.js
+++ src/obj_placers/window_placer.js
@@ -1,17 +1,14 @@
-
-
 /**
  * Will probably be made generic soon, but for now the idea is to place as wide
  * a window as possible on each WPSCP and with padding.
  */
-function WindowPlacer(hPadding, vBottom, vTop) {
+function RectangularWindowPlacer(hPadding, vBottom, vTop) {
   this.hPad = hPadding;
   this.vBottom = vBottom;
   this.vTop = vTop;
 }
-WindowPlacer.prototype = {
+RectangularWindowPlacer.prototype = {
   placeOnSegment: function(seg) {
-
     var objLength = seg.length - this.hPad * 2;
     var lowStart = new THREE.Vector2(0, this.vBottom);
     var lowEnd = new THREE.Vector2(objLength, this.vBottom);
@@ -29,4 +26,38 @@ WindowPlacer.prototype = {
   }
 }
 
-module.exports = { WindowPlacer };
+function RoundWindowPlacer(hPadding, vBottom, vTop) {
+  this.hPad = hPadding;
+  this.vBottom = vBottom;
+  this.vTop = vTop;
+}
+RoundWindowPlacer.prototype = {
+  placeOnSegment: function(seg) {
+    var hRadius = (seg.length - this.hPad * 2) / 2;
+    var hCenter = hRadius;
+    var vRadius = (this.vTop - this.vBottom) / 2
+    var vCenter = this.vBottom + vRadius;
+
+    seg.objects.push({
+      start: this.hPad,
+      end: seg.length - this.hPad,
+      bottomCurve: new THREE.EllipseCurve(
+        hCenter, vCenter,
+        hRadius, vRadius,
+        Math.PI, Math.PI * 2,
+        false, 0
+      ),
+      topCurve: new THREE.EllipseCurve(
+        hCenter, vCenter,
+        hRadius, vRadius,
+        Math.PI, 0,
+        true, 0
+      ),
+      minPoints: 16,
+      maxPoints: 16
+    });
+  }
+}
+
+
+module.exports = { WindowPlacer: RoundWindowPlacer };
